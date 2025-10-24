86a0f87e41c9c765b88f271f21fda2c6cdbe2eea
beastmodesky12 <sprince2@ggc.edu>
2025-10-24 13:13:09 -0400

    Resolve merge conflict between master and parallel-branch for script3.sh

--- 04-life.sh
+++ 04-life.sh
@@ -15,3 +15,4 @@ fi
 # exercise: write a script that prints whether it is 
 # morning or not
 echo "Edit B: SKYLOR PRINCE - parallel universe - $(date +%F)"
+echo "Final: SKYLOR PRINCE - merged universe - 2025-10-24"
